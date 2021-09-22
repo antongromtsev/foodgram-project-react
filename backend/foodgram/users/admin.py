ea9ab658a95f2dd46c522d6b7c95fb0a1753dbc5
Anotn Gromtsev <fenix217grom@.gmail.com>
2021-09-23 00:18:24 +0300

    Админка

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MyUser
+
+class MyUserAdmin(admin.ModelAdmin):
+    list_display = ("email", "username", "role")
+    search_fields = ("username",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(MyUser, MyUserAdmin)
