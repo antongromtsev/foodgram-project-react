fda88828ee22747f5bebc17dca30b83e150ca3bd
AntonGromtsev <fenix217grom@gmail.com>
2021-10-08 23:29:50 +0300

    10

--- backend/foodgram/recipes/filters.py
+++ backend/foodgram/recipes/filters.py
@@ -1,6 +1,8 @@
 import django_filters
 
-from .models import Ingredient, Recipe
+from django_filters import rest_framework as filters
+
+from .models import Ingredient, Recipe, Tag
 
 
 class IngredientFilter(django_filters.FilterSet):
@@ -16,9 +18,10 @@ class RecipeFilter(django_filters.FilterSet):
         field_name='author__pk',
         lookup_expr='contains',
     )
-    tags = django_filters.CharFilter(
+    tags = filters.filters.ModelMultipleChoiceFilter(
+        queryset=Tag.objects.all(),
         field_name='tags__slug',
-        lookup_expr='contains',
+        to_field_name='slug',
     )
     is_favorited = django_filters.BooleanFilter(method='filter')
     is_in_shopping_cart = django_filters.BooleanFilter(method='filter')
