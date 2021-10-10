38aa1728bb8da2e9897822e24e9b0653098106d8
AntonGromtsev <fenix217grom@gmail.com>
2021-10-11 01:43:42 +0300

    4

--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -2,12 +2,12 @@ from django.db.models import Sum
 from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters import rest_framework as filters
+from recipes.serializer import RecipeSubscriptionsSerializer
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from recipes.serializer import RecipeSubscriptionsSerializer
 from users.models import Favourites, ShoppingCart
 
 from .filters import IngredientFilter, RecipeFilter
